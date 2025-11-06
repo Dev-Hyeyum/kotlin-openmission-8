618d43fcdc2aeb8b4f069eaad263cfa0c6d79d37
cbnuLeehyunwoo <henow123@naver.com>
2025-11-07 01:10:41 +0900

    build(server): JSON 직렬화를 위한 content-negotiation 및 kotlinx-json 의존성 추가

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,13 +3,10 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.jvm)
     // 2. Ktor 플러그인 적용
     alias(libs.plugins.ktor)
+    kotlin("plugin.serialization") version libs.versions.kotlin.get()
 }
 group = "com.kotlinopenmission8.server"
 version = "0.0.1"
-
-val ktor_version = "3.3.1"
-val logback_version = "1.5.18"
-val kotlin_version = "2.2.20"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
@@ -22,19 +19,14 @@ kotlin {
 }
 
 dependencies {
+    implementation("io.ktor:ktor-server-content-negotiation:${libs.versions.ktor.get()}")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:${libs.versions.ktor.get()}")
+    implementation("io.ktor:ktor-server-status-pages:${libs.versions.ktor.get()}")
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
-    implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.config.yaml)
+    // 테스트 의존성
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
-    // The new dependency to be added
-    implementation("io.ktor:ktor-server-status-pages:${ktor_version}")
-    // The existing dependencies
-    implementation("io.ktor:ktor-server-core-jvm:${ktor_version}")
-    implementation("io.ktor:ktor-server-netty-jvm:${ktor_version}")
-    implementation("ch.qos.logback:logback-classic:${logback_version}")
-    testImplementation("io.ktor:ktor-server-test-host-jvm:${ktor_version}")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:${kotlin_version}")
 }
