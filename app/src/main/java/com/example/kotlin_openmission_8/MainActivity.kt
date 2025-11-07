a3bc795b2012e34c102f6740ba7091a736717204
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-11-07 15:49:22 +0900

    feat: main함수 조정

--- app/src/main/java/com/example/kotlin_openmission_8/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_openmission_8/MainActivity.kt
@@ -1,32 +1,63 @@
 package com.example.kotlin_openmission_8
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kotlin_openmission_8.ui.theme.Kotlinopenmission8Theme
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.example.kotlin_openmission_8.components.CreateButton
+import com.example.kotlin_openmission_8.components.Header
+import io.ktor.client.*
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.coroutines.CoroutineScope
 
 class MainActivity : ComponentActivity() {
+
+    private val client = HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MainScreen(modifier = Modifier.fillMaxSize())
+            val coroutineScope = rememberCoroutineScope()
+            val context = LocalContext.current
+
+            MyApp(context, coroutineScope, client)
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun MainScreenPreview() {
-    Kotlinopenmission8Theme {
-        MainScreen(modifier = Modifier.fillMaxSize())
+fun MyApp(context: Context, coroutineScope: CoroutineScope, client: HttpClient) {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Header(
+                "어쩌구",
+                Modifier.fillMaxWidth().height(50.dp)
+            )
+            CreateButton(coroutineScope, context, client, "텍스트")
+            CreateButton(coroutineScope, context, client, "버튼")
+        }
+
     }
 }
