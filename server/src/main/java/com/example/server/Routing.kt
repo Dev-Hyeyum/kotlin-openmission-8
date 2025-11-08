95a8ee7c55f68891a32426aea8cefcaf3d8762a2
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2025-11-08 14:36:59 +0900

    feat: Routing에 delete 추가

--- server/src/main/java/com/example/server/Routing.kt
+++ server/src/main/java/com/example/server/Routing.kt
@@ -7,6 +7,7 @@ import io.ktor.server.application.Application
 import io.ktor.server.request.receive
 import io.ktor.server.response.respond
 import io.ktor.server.response.respondText
+import io.ktor.server.routing.delete
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
@@ -16,28 +17,38 @@ fun Application.configureRouting() {
         get("/") {
             call.respondText("Hello World!")
         }
-        // 기존 GET 요청
+
         get("/components") {
             val components = ComponentRepository.getAll()
-            // (참고: mapOf("hello" to components) 대신 리스트를 바로 보내는 것이 더 일반적입니다.)
             call.respond(components)
+            return@get
         }
 
-        // [핵심] POST로 JSON을 받아 컴포넌트 추가
         post("/components") {
             try {
-                // 1. 클라이언트가 보낸 JSON을 Component 객체로 자동 변환 (수신)
                 val newComponent = call.receive<Component>()
-
-                // 2. Repository에 추가
                 ComponentRepository.addComponents(newComponent)
-
-                // 3. 성공 응답 (생성됨)
                 call.respond(HttpStatusCode.Created, "Component added successfully")
-
+                return@post
             } catch (e: Exception) {
-                // 4. 실패 시 (예: JSON 형식이 안 맞을 때)
                 call.respond(HttpStatusCode.BadRequest, "Invalid component format")
+            }
+        }
+
+        delete("/{id}") {
+            try{
+                val id = call.parameters["id"]
+                if (id == null) {
+                    call.respond(HttpStatusCode.BadRequest)
+                    return@delete
+                }
+                if (ComponentRepository.deleteComponent(id)) {
+                    call.respond(HttpStatusCode.NoContent)
+                } else {
+                    call.respond(HttpStatusCode.NotFound)
+                }
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.BadRequest, "${e.message}")
             }
         }
 
