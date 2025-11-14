7f160fbd0899289fc57519cb83c2f32755f9f2b8
cbnuLeehyunwoo <henow123@naver.com>
2025-11-14 17:28:42 +0900

    feat(server): 켄버스를 생성하고 Map으로 관리하는 싱글톤 객체 구현
    캔버스 매니저 역할을 수행하는 싱글톤 객체를 구현하였습니다. 해당 객체는
    6자리의 roodId를 만들고 부여합니다.

---
+++ server/src/main/java/com/example/server/CanvasManger.kt
@@ -0,0 +1,19 @@
+package com.example.server
+
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+
+// Room 인스턴스를 생성하고 맵으로 관리하는 싱글톤 객체
+object CanvasManger {
+    // 멀티쓰레드 환경에 특화된 HashMap
+    private val rooms = ConcurrentHashMap<String, Room>()
+
+    fun createCanvas(): String {
+        val roomId = UUID.randomUUID().toString().take(6) // 6자리의 랜덤 아이디
+        val newRoom = Room()
+        rooms[roomId] = newRoom
+        println("새 캔버스 생성됨 $roomId")
+        return roomId
+    }
+
+}
