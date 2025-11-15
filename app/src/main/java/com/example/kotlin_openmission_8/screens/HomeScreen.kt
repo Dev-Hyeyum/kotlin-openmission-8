b6608c4b38caec752f7ec62685a6cd25f02de962
cbnuLeehyunwoo <henow123@naver.com>
2025-11-15 23:21:48 +0900

    feat(app): 홈 화면에서 RoomId가 생성되면 해당 캔버스 스크린으로 자동으로 리다이렉션 해주는 기능 구현
    HomeScreen에서 ViewModel을 파라미터로 받게 한 뒤, View모델의 roomId를
    관찰하고, roomId가 생기면 해당 캔버스 화면으로 이동하게 하는 로직을
    추가하였습니다.
    
    해당 로직을 통해 새로운 캔버스를 만들면 roomId가 생성되고 자동으로 해당
    화면으로 이동될 것을 기대해볼 수 있습니다.

--- app/src/main/java/com/example/kotlin_openmission_8/screens/HomeScreen.kt
+++ app/src/main/java/com/example/kotlin_openmission_8/screens/HomeScreen.kt
@@ -15,15 +15,30 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.kotlin_openmission_8.model.Components
 
 @Composable
-fun HomeScreen(onNavigateToDetail: () -> Unit) {
+fun HomeScreen(
+    // viewModel을 매개변수로
+    viewModel: Components,
+    onNavigateToDetail: () -> Unit
+) {
+    //ViewModel의 roomId를 관찰하도록
+    val roomId by viewModel.currentRoomId.collectAsState()
+    LaunchedEffect(roomId) {
+        if(roomId != null) {
+            onNavigateToDetail()
+        }
+    }
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
@@ -32,10 +47,12 @@ fun HomeScreen(onNavigateToDetail: () -> Unit) {
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Text("서비스 이름이 들어가면 좋을 듯(title)",)
+            Text("서비스 이름이 들어가면 좋을 듯(title)")
             // 캔버스 생성 및 삭제 바
-            Row (
-                modifier = Modifier.fillMaxWidth().border(2.dp, Color.Black, ),
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .border(2.dp, Color.Black),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.End
             ) {
@@ -71,5 +88,5 @@ fun HomeScreen(onNavigateToDetail: () -> Unit) {
 @Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
-    HomeScreen(onNavigateToDetail={})
+    HomeScreen(onNavigateToDetail = {})
 }
