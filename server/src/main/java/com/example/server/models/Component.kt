c9ad04336132bf4780404a906f0dc7e1f6ed324c
cbnuLeehyunwoo <henow123@naver.com>
2025-11-21 04:26:32 +0900

    refactor(server): 컴포넌트에 이벤트를 매핑할 EventAction 클래스 리팩터링
    이벤트의 대상이 될 타겟아이디를 추가하고, 기존의 단일 이벤트만 가질 수 있던 구현에서
    컴포넌트를 여러 이벤트와 연결할 수 있게 하기 위해 리스트를 갖도록 수정하였습니다.

--- server/src/main/java/com/example/server/models/Component.kt
+++ server/src/main/java/com/example/server/models/Component.kt
@@ -43,6 +43,6 @@ data class Component(
         borderColor = "#FF000000",
         borderRadius = 0.0f,
     ),
-    val onClickAction: EventAction? = null,
+    val actions: List<EventAction> = emptyList(),
     val imageUrl: String? = null
 )
